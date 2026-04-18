7861ee659992af741d8cb3a1f89346cddc915559
Skyy Moore <mooreapps2022@gmail.com>
2026-04-17 17:48:33 -0700

    Moved around assets.

--- assets/covers/resize.sh
+++
@@ -1,9 +0,0 @@
-file_name=$1
-file_ext=$2
-
-convert "$file_name.$file_ext" -resize 180x270^ -gravity center -extent 180x270 -quality 75 "$file_name.webp"
-
-mv "$file_name.webp" keep
-rm "$file_name.$file_ext"
-clear
-ls
