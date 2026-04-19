1a9a38273fd664e0e4e4994606b33f8cd29d77f1
Skyy Moore <mooreapps2022@gmail.com>
2026-04-18 23:59:11 -0700

    Added a popup box when you click a book card, which displays all the properties of the book object.

--- app.js
+++ app.js
@@ -2,6 +2,68 @@
 
 let currentSort = "default";
 
+function openBookPopup(book) {
+	let	popupOverlay = document.querySelector(".popupOverlay");
+
+	if (!popupOverlay) {
+		popupOverlay = document.createElement("div");
+		popupOverlay.className = "popupOverlay";
+		document.body.append(popupOverlay);
+	}
+
+	popupOverlay.innerHTML = "";
+	
+	const	popupBox = document.createElement("button");
+	popupBox.className = "popupBox";
+
+	const	closeButton = document.createElement("button");
+	closeButton.className = "popupClose";
+	closeButton.type = "button";
+	closeButton.textContent = "x";
+	
+	const	img = document.createElement("img");
+	img.src = book.coverImage;
+	img.alt = `Cover of ${book.title}`;
+	img.width = 180;
+
+	const	title = document.createElement("h2");
+	title.textContent = book.title;
+
+	const	author = document.createElement("p");
+	author.innerHTML = `<strong>Author:</strong> ${book.author}`;
+
+	const	publicationDate = document.createElement("p");
+	publicationDate.innerHTML = `<strong>Publication Date:</strong> ${book.publicationDate}`;
+
+	const	description = document.createElement("p");
+	description.innerHTML = `<strong>Description:</strong> ${book.description}`;
+
+	const	quote = document.createElement("p");
+	quote.innerHTML = `<strong>Quote:</strong> "${book.quote}"`;
+
+	closeButton.addEventListener("click", () => {
+		popupOverlay.remove();
+	});
+
+	popupOverlay.addEventListener("click", (event) => {
+		if (event.target === popupOverlay) {
+			popupOverlay.remove();
+		}
+	});
+
+	popupBox.append(
+		closeButton,
+		img,
+		title,
+		author,
+		publicationDate,
+		description,
+		quote
+	);
+
+	popupOverlay.append(popupBox);
+}
+
 function getRenderedBooks() {
 	const visibleBooks = books.filter((books) => books.show);
 
@@ -46,6 +108,10 @@ function renderBooks(books) {
 		img.width = 180;
 
 		bookCard.append(img);
+		bookCard.addEventListener("click", () => {
+			openBookPopup(book);
+		});
+
 		bookGrid.appendChild(bookCard);
 	});
 }
